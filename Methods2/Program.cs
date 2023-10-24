014ecf71f9e18489cac8a613f360c12dc0a84991
Sabır Akbana <s.akbana9o@gmail.com>
2023-10-24 15:44:07 +0300

    comments added

--- Methods2/Program.cs
+++ Methods2/Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine(Multiply(5, 6));
             Console.WriteLine(Multiply(5, 6, 7));
         }
+
+        /* Method Overloading */
         static int Multiply(int x, int y)
         {
             return x * y;
